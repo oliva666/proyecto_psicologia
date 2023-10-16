58775c3c77542a1449f0adaa7743611adc89c7cc
oliva666 <oliva.9920@gmail.com>
2023-10-16 02:33:02 -0600

    se agrego la vista cita

---
+++ lib/Views/cita_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class CitaView extends StatefulWidget {
+  const CitaView({ Key? key }) : super(key: key);
+
+  @override
+  _CitaViewState createState() => _CitaViewState();
+}
+
+class _CitaViewState extends State<CitaView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
